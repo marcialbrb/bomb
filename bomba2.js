82d92dca66d0cb8f870bb2c05e1406f9a10a001b
Marcial Barboza <68023873+marcialbrb@users.noreply.github.com>
2020-10-15 15:07:51 -0300

    Add files via upload

---
+++ bomba2.js
@@ -0,0 +1,58 @@
+alert("Tienes 3 segundos para desactivar la bomba");
+    
+var timer = setTimeout(bomba, 3000);
+
+function bomba (){
+    alert("BOOOOOOM");
+    postClick();
+}
+
+var desactivar = document.getElementById("cableAmarillo");
+desactivar.addEventListener("click", clickDesactivar);
+
+function clickDesactivar (){
+    clearTimeout(timer);
+    alert("Bomba desactivada, buen trabajo!");
+    desactivar.removeEventListener("click",clickDesactivar)
+    explota.removeEventListener("click", clickExplota);
+    explota2.removeEventListener("click", clickExplota);
+    postClickCumplido();
+
+}
+
+explota = document.getElementById("cableRojo");
+explota2 = document.getElementById("cableVerde");
+
+explota.addEventListener("click", clickExplota);
+explota2.addEventListener("click", clickExplota);
+
+function clickExplota () {
+    clearTimeout(timer);
+    alert("BOOOOOOOM");
+
+    postClick();
+}
+
+function postClick(){
+    explota.removeEventListener("click",clickExplota)
+    explota2.removeEventListener("click",clickExplota)
+    desactivar.removeEventListener("click",clickDesactivar);
+    explota.addEventListener("click", clickFin);
+    explota2.addEventListener("click", clickFin);
+    desactivar.addEventListener("click", clickFin);
+}
+
+function clickFin() {
+    alert("La bomba ha explotado, morimos todos :(")
+}
+
+function postClickCumplido(){
+    explota.addEventListener("click", clickCumplido);
+    explota2.addEventListener("click", clickCumplido);
+    desactivar.addEventListener("click",clickCumplido);
+}
+
+function clickCumplido(){
+    alert("La bomba ya fue desactivada, estamos vivos gracias a ti :)")
+}
+
