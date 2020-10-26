fb4841f5b09ac24fbc407b8ba96f2d5180103470
Marcial Barboza <68023873+marcialbrb@users.noreply.github.com>
2020-10-26 13:13:25 -0300

    Update and rename bomba2.js to bomba.js

--- bomba.js
+++ bomba.js
@@ -1,10 +1,20 @@
-alert("Tienes 5 segundos para desactivar la bomba");
+var reloj = document.getElementsByClassName("reloj")[0];
+var temp = 5;                                              //Temporizador inica en 5 segundos
+
+var timerTemp = setInterval(mostrarTemp,1000)                              //Realizar la funcion mostrarTemp cada 1000ms
+function mostrarTemp() { 
     
+    if (temp >= 0) {
+        reloj.innerHTML = "00:"+"0"+temp;                   //Primero muestro el temp en 5 segundos
+        temp = temp - 1;                                    //Luego le resto uno para el prox ciclo mostrar 4 segundos
+    }
+}
+alert("Tienes 5 segundos para desactivar la bomba");
 var timer = setTimeout(bomba, 5000);
 
 function bomba (){
     alert("BOOOOOOM");
-    alert("Todos morimos, no hiciste nada :(")
+    alert("Todos morimos, POR QUÉ NO HICISTE NADA? :(")
     postClick();
 }
 
@@ -13,6 +23,7 @@ desactivar.addEventListener("click", clickDesactivar);
 
 function clickDesactivar (){
     clearTimeout(timer);
+    clearTimeout(timerTemp);
     alert("Bomba desactivada, buen trabajo!");
     desactivar.removeEventListener("click",clickDesactivar)
     explota.removeEventListener("click", clickExplota);
@@ -29,6 +40,7 @@ explota2.addEventListener("click", clickExplota);
 
 function clickExplota () {
     clearTimeout(timer);
+    clearTimeout(timerTemp);
     alert("BOOOOOOOM");
 
     postClick();
@@ -56,4 +68,3 @@ function postClickCumplido(){
 function clickCumplido(){
     alert("La bomba ya fue desactivada, estamos vivos gracias a ti :)")
 }
-
