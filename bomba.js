18dc9c2c065bb16d5cb03d0989addd416a72ebce
Marcial Barboza <68023873+marcialbrb@users.noreply.github.com>
2020-10-27 14:27:42 -0300

    Update bomba.js

--- bomba.js
+++ bomba.js
@@ -1,7 +1,15 @@
-var reloj = document.getElementsByClassName("reloj")[0];
-var temp = 5;                                              //Temporizador inica en 5 segundos
+window.onload = getRndInteger(0,3);                        //Carga la página y se elige un número al azar que determina cual será el desactivador
+                                                           //Los numeros estan entren 0 y 3, especificamente 0,1,2.
+function getRndInteger(min, max) {
+    let numero = Math.floor(Math.random() * (max - min)) + min;
+    return numbRandom(numero)                                  //Como numero lo necesito para determinar el desactiv, lo mando a la funcion que
+                                                               //se encarga de determinar que numero corresponde a el desactivador
+}
 
-var timerTemp = setInterval(mostrarTemp,1000)                              //Realizar la funcion mostrarTemp cada 1000ms
+let reloj = document.getElementsByClassName("reloj")[0];
+let temp = 5;                                                   // Inicio el temporizador en 5 segundos
+
+let timerTemp = setInterval(mostrarTemp,1000)                              //Realizar la funcion mostrarTemp cada 1000ms
 function mostrarTemp() { 
     
     if (temp >= 0) {
@@ -9,17 +17,52 @@ function mostrarTemp() {
         temp = temp - 1;                                    //Luego le resto uno para el prox ciclo mostrar 4 segundos
     }
 }
-alert("Tienes 5 segundos para desactivar la bomba");
-var timer = setTimeout(bomba, 5000);
+
+alert("Tienes 5 segundos para desactivar la bomba");        //Alerta que indica instrucciones
+let timer = setTimeout(bomba, 5000);                        //Inicia el tiempo para que la bomba explote (Esta sincronizado con el temporizador)
+
+
+
+function numbRandom (numero){
+    if(numero==0) {
+        desactivar = document.getElementById("cableAmarillo");
+        desactivar.addEventListener("click", clickDesactivar);
+    
+        explota = document.getElementById("cableRojo");
+        explota2 = document.getElementById("cableVerde");
+
+        explota.addEventListener("click", clickExplota);
+        explota2.addEventListener("click", clickExplota);
+    }
+
+    if(numero==1){
+        desactivar = document.getElementById("cableVerde");
+        desactivar.addEventListener("click", clickDesactivar);
+    
+        explota = document.getElementById("cableRojo");
+        explota2 = document.getElementById("cableAmarillo");
+
+        explota.addEventListener("click", clickExplota);
+        explota2.addEventListener("click", clickExplota);
+    }
+
+    if(numero==2){
+        desactivar = document.getElementById("cableRojo");
+        desactivar.addEventListener("click", clickDesactivar);
+    
+        explota = document.getElementById("cableAmarillo");
+        explota2 = document.getElementById("cableVerde");
+
+        explota.addEventListener("click", clickExplota);
+        explota2.addEventListener("click", clickExplota);
+    }
+}
 
 function bomba (){
     alert("BOOOOOOM");
     alert("Todos morimos, POR QUÉ NO HICISTE NADA? :(")
     postClick();
 }
-
-var desactivar = document.getElementById("cableAmarillo");
-desactivar.addEventListener("click", clickDesactivar);
 
 function clickDesactivar (){
     clearTimeout(timer);
@@ -31,12 +74,6 @@ function clickDesactivar (){
     postClickCumplido();
 
 }
-
-explota = document.getElementById("cableRojo");
-explota2 = document.getElementById("cableVerde");
-
-explota.addEventListener("click", clickExplota);
-explota2.addEventListener("click", clickExplota);
 
 function clickExplota () {
     clearTimeout(timer);
